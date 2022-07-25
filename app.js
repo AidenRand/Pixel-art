3a4c8a3f5d4df9e56cec73c43a1d98efc404ef76
Cortrax <aidenrphotos@gmail.com>
2022-07-25 12:54:57 -0400

    disable grid

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const container = document.querySelector(".container");
 const sizeElement = document.querySelector(".size");
 const color = document.querySelector(".color");
-const reset = document.querySelector('.reset')
-const size = sizeElement.value;
-
+const resetBtn = document.querySelector(".reset");
+let size = sizeElement.value;
 let draw = false;
 
-//
 function drawCanvas(size) {
+    // Set size of grid
   container.style.setProperty("--size", size);
   //Create number of pixels in grid
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.classList.add("pixel");
 
-    // Change color of grid on mouse press
+    // Change color of grid
     div.addEventListener("mouseover", function () {
       if (!draw) return;
       div.style.backgroundColor = color.value;
@@ -28,6 +27,7 @@ function drawCanvas(size) {
   }
 }
 
+// Keep drawing only while mouse is pressed down
 window.addEventListener("mousedown", function () {
   draw = true;
 });
@@ -35,9 +35,18 @@ window.addEventListener("mouseup", function () {
   draw = false;
 });
 
-reset.addEventListener("click", function () {
+// Reset grid on button press
+function reset() {
   container.innerHTML = "";
   drawCanvas(size);
+}
+
+resetBtn.addEventListener("click", reset);
+
+// Change grid size
+sizeElement.addEventListener("keyup", function () {
+  size = sizeElement.value;
+  reset();
 });
 
 drawCanvas(size);
